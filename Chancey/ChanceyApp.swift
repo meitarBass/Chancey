6edea5039e40677d9ac6a5c2bf9d3e1933f4d03f
meitarbasson <meitarbass@gmail.com>
2022-07-03 01:56:28 +0300

    Initial Commit

---
+++ Chancey/ChanceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChanceyApp.swift
+//  Chancey
+//
+//  Created by Meitar Basson on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChanceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
